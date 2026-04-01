065bfe6e1e70959b26206651f95d817569f1e899
2501129 <303076898@qq.com>
2026-04-01 17:11:25 +0900

    Reformat WaterOffice.java for consistency

--- WEEK5/Water4/src/WaterOffice.java
+++ WEEK5/Water4/src/WaterOffice.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class WaterOffice {
     private Customer[] customers;
 
-    // 생성자: PPT Main에서 정확히 이렇게 호출함
+    
     public WaterOffice(Customer[] customers) {
         this.customers = customers;
     }
 
-    // ✅ inputData: Scanner를 한 번만 생성해서 전달
+    
     public void inputData() throws IOException {
         Scanner scanner = new Scanner(System.in);
         for (Customer c : customers) {
@@ -18,7 +18,7 @@ public class WaterOffice {
         scanner.close();
     }
 
-    // ✅ display: PPT와 완전 일치
+   
     public void display() {
         System.out.println("\n=== 수도 요금 청구 내역 ===");
         System.out.println("수도번호\t이름\t유형\t사용량\t사용요금\t세금\t총청구금액");

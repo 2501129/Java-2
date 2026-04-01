0d54f4e596d47b47ed1135eaa1e3b0286898d4d5
2501129 <303076898@qq.com>
2026-04-01 17:11:02 +0900

    Update Type.java

--- WEEK5/Water4/src/Type.java
+++ WEEK5/Water4/src/Type.java
@@ -1,19 +1,19 @@
 import java.io.IOException;
 
 public class Type {
-    // 通用属性：类型名、单价、税率
+    
     private String typeName;
     private int unitPrice;
     private float taxRate;
 
-    // 父类构造器，子类通过super调用
+   
     public Type(String typeName, int unitPrice, float taxRate) {
         this.typeName = typeName;
         this.unitPrice = unitPrice;
         this.taxRate = taxRate;
     }
 
-    // Getter方法，供Customer类调用
+   
     public String getTypeName() {
         return typeName;
     }
